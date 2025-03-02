d34e2886843a112bd0690325763b08bd21a1d3c6
IwantHappiness <magomadovimran142@gmail.com>
2025-03-02 19:27:11 +0000

    fix handler input

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ fn main() {
         println!("Введите 0 если хотите выйти.\n");
         print!("Выберете формат: ");
 
-        // Сбрасываю буффер
+        // Сбрасывание буффера
         io::stdout().flush().unwrap();
 
-        // Читаю ввод
+        // Обработка ввода
         let mut format = String::new();
         io::stdin().read_line(&mut format).expect("Failed input.");
 
         // Парсинг ввода
         if let Ok(format) = format.trim().parse::<i32>() {
             match format {
-                // Выходим из цикла
+                // Выход из цикла
                 0 => break,
-                // Печатаю цвет в RGB формате
+                // Печать цвета в RGB формате
                 1 => {
                     let rgb = rand_rgb();
                     println!(
@@ -34,16 +34,18 @@ fn main() {
                     );
                 }
 
-                // Печаю цвет в HEX формате
-                _ => println!("Color in hex: \x1b[95m{}\x1b[0m\n", rand_hex()),
+                // Печать цвета в HEX формате
+                2 => println!("Color in hex: \x1b[95m{}\x1b[0m\n", rand_hex()),
+                // Обработка недопустимых значений
+                _ => eprintln!("\x1b[31mНеверный формат ввода.\x1b[0m\n"),
             }
         } else {
-            println!("\x1b[31mНеверный формат ввода.\x1b[0m\n");
+            eprintln!("\x1b[31mНеверный формат ввода.\x1b[0m\n");
         };
     }
 }
 
-// Создаю цвет в RGB формате
+// Создание цвета в RGB формате
 fn rand_rgb() -> ColorRgb {
     ColorRgb(
         rng().random_range(1..=256),
@@ -52,14 +54,14 @@ fn rand_rgb() -> ColorRgb {
     )
 }
 
-// Создаю цвет в HEX формате
+// Создание цвета в HEX формате
 fn rand_hex() -> String {
     let mut hex = String::from("#");
     let hex_mas = [
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
     ];
 
-    // Добавляю пока длинна строки не будут 7
+    // Добавлять пока длинна строки не будут 7
     while hex.len() < 7 {
         let index = rng().random_range(0..=15);
         hex.push_str(hex_mas[index]);
